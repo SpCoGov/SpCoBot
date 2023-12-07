1f9cccc2d440c78978d92aef0a7a379c70ef8482
SpCo <haimiancouna@gmail.com>
2023-12-07 22:42:09 +0800

    add notes

--- src/main/java/top/spco/service/command/CommandMeta.java
+++ src/main/java/top/spco/service/command/CommandMeta.java
@@ -71,36 +71,58 @@ public class CommandMeta {
         if (context.startsWith(CommandSystem.COMMAND_START_SYMBOL)) {
             List<String> partList = new ArrayList<>();
             StringBuilder currentArgument = new StringBuilder();
+            // 标志：是否在引号内
             boolean inQuote = false;
+            // 标志：是否处于转义状态
             boolean inEscape = false;
+            // 标志：是否处于后引号的后一个字符
             boolean afterQuoteClose = false;
-            while (canRead()) {
-                boolean skip = false;
+            int commandLength = command.length();
+            // 循环迭代命令字符串中的每个字符
+            while (cursor + 1 <= commandLength) {
+                boolean isAfterQuoteClose = false;
                 final char next = peek();
+                // 如果处于转义状态，直接将字符添加到当前参数中
                 if (inEscape) {
                     currentArgument.append(next);
+                    // 退出转义状态
                     inEscape = false;
                 } else {
+                    // 如果下一个字符是双引号
                     if (next == SYNTAX_DOUBLE_QUOTE) {
+                        // 如果在引号内，则说明将要退出引号状态。
+                        // 这里的 isAfterQuoteClose 和 afterQuoteClose 用于检测后引号的后一个字符是否为参数分隔符
                         if (inQuote) {
-                            skip = true;
+                            isAfterQuoteClose = true;
                         }
+                        // 切换引号状态
                         inQuote = !inQuote;
+                        // 如果下一个字符是转义字符
                     } else if (next == SYNTAX_ESCAPE) {
+                        // 进入转义状态
                         inEscape = true;
+                        // 如果下一个字符是参数分隔符
                     } else if (next == CommandSystem.ARGUMENT_SEPARATOR_CHAR) {
+                        // 如果在引号内
                         if (inQuote) {
+                            // 将字符添加到当前参数中
                             currentArgument.append(next);
                         } else {
+                            // 添加当前参数到参数列表中
                             partList.add(currentArgument.toString());
-                            currentArgument = new StringBuilder();
+                            // 清空当前参数构建器
+                            currentArgument.setLength(0);
                         }
                     } else {
+                        // 将字符添加到当前参数中
                         currentArgument.append(next);
                     }
                 }
+                // 如果在引号关闭后且下一个字符不是参数分隔符
                 if (afterQuoteClose && next != CommandSystem.ARGUMENT_SEPARATOR_CHAR) {
+                    // 将字符添加到当前参数中
                     currentArgument.append(next);
+                    // 添加当前参数到参数列表中
                     partList.add(currentArgument.toString());
                     String[] parts = partList.toArray(new String[0]);
                     // 标签转换为小写
@@ -111,7 +133,7 @@ public class CommandMeta {
                     System.arraycopy(parts, 1, args, 0, parts.length - 1);
                     throw CommandSyntaxException.expectedSeparator(this.label, args, args.length - 1);
                 }
-                afterQuoteClose = skip;
+                afterQuoteClose = isAfterQuoteClose;
                 skip();
             }
             partList.add(currentArgument.toString());
@@ -122,21 +144,18 @@ public class CommandMeta {
             String[] args = new String[parts.length - 1];
             // 将parts数组中从第二个元素开始的所有元素复制到新的数组中
             System.arraycopy(parts, 1, args, 0, parts.length - 1);
+            // 如果还处于引号内
             if (inQuote) {
-                throw CommandSyntaxException.error("需要\"", this.label, args, args.length - 1);
+                throw CommandSyntaxException.error("需要后引号(\")", this.label, args, args.length - 1);
             }
+            // 移除参数中的空字符串
             this.args = removeEmptyStrings(args);
         }
     }
 
-    public boolean canRead(final int length) {
-        return cursor + length <= command.length();
-    }
-
-    public boolean canRead() {
-        return canRead(1);
-    }
-
+    /**
+     * 读取光标位置的字符
+     */
     public char peek() {
         return command.charAt(cursor);
     }
@@ -145,6 +164,9 @@ public class CommandMeta {
         return command.charAt(cursor++);
     }
 
+    /**
+     * 将光标后移一位
+     */
     public void skip() {
         cursor++;
     }
@@ -237,14 +259,14 @@ public class CommandMeta {
     /**
      * 获取命令的目标用户id类型参数
      *
-     * @param index 参数的索引
+     * @param index   参数的索引
      * @param message 发送的消息
      * @return 对应参数的值
      * @throws CommandSyntaxException 索引超出了参数数组的范围或该参数不是预期类型
      */
     public long targetUserIdArgument(int index, Message message) throws CommandSyntaxException {
         try {
-          return userIdArgument(index);
+            return userIdArgument(index);
         } catch (Exception e) {
             var quote = SpCoBot.getInstance().getMessageService().getQuote(message);
             if (quote == null) {
