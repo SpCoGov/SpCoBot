9b69e95d0611973d0b25b85503b39fc4986ae1fb
SpCo <haimiancouna@gmail.com>
2023-12-30 00:23:34 +0800

    Delete some unused code

--- src/main/java/top/spco/util/Validate.java
+++ src/main/java/top/spco/util/Validate.java
@@ -18,7 +18,6 @@ package top.spco.util;
 
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.Map;
 import java.util.Objects;
 import java.util.function.Supplier;
 import java.util.regex.Pattern;
@@ -57,7 +56,6 @@ public class Validate {
             "The value %s is not in the specified inclusive range of %s to %s";
     private static final String DEFAULT_MATCHES_PATTERN_EX = "The string %s does not match the pattern %s";
     private static final String DEFAULT_IS_NULL_EX_MESSAGE = "The validated object is null";
-    private static final String DEFAULT_IS_TRUE_EX_MESSAGE = "The validated expression is false";
     private static final String DEFAULT_NO_NULL_ELEMENTS_ARRAY_EX_MESSAGE =
             "The validated array contains null element at index: %d";
     private static final String DEFAULT_NO_NULL_ELEMENTS_COLLECTION_EX_MESSAGE =
@@ -66,8 +64,6 @@ public class Validate {
     private static final String DEFAULT_NOT_EMPTY_ARRAY_EX_MESSAGE = "The validated array is empty";
     private static final String DEFAULT_NOT_EMPTY_CHAR_SEQUENCE_EX_MESSAGE =
             "The validated character sequence is empty";
-    private static final String DEFAULT_NOT_EMPTY_COLLECTION_EX_MESSAGE = "The validated collection is empty";
-    private static final String DEFAULT_NOT_EMPTY_MAP_EX_MESSAGE = "The validated map is empty";
     private static final String DEFAULT_VALID_INDEX_ARRAY_EX_MESSAGE = "The validated array index is invalid: %d";
     private static final String DEFAULT_VALID_INDEX_CHAR_SEQUENCE_EX_MESSAGE =
             "The validated character sequence index is invalid: %d";
@@ -83,120 +79,10 @@ public class Validate {
     public Validate() {
     }
 
-    /**
-     * Validate that the argument condition is {@code true}; otherwise
-     * throwing an exception with the specified message. This method is useful when
-     * validating according to an arbitrary boolean expression, such as validating a
-     * primitive number or using your own custom validation expression.
-     *
-     * <pre>Validate.isTrue(i &gt; 0.0, "The value must be greater than zero: &#37;d", i);</pre>
-     *
-     * <p>For performance reasons, the long value is passed as a separate parameter and
-     * appended to the exception message only in the case of an error.</p>
-     *
-     * @param expression the boolean expression to check
-     * @param message    the {@link String#format(String, Object...)} exception message if invalid, not null
-     * @param value      the value to append to the message when invalid
-     * @throws IllegalArgumentException if expression is {@code false}
-     * @see #isTrue(boolean)
-     * @see #isTrue(boolean, String, double)
-     * @see #isTrue(boolean, String, Object...)
-     */
-    public static void isTrue(final boolean expression, final String message, final long value) {
-        if (!expression) {
-            throw new IllegalArgumentException(String.format(message, Long.valueOf(value)));
-        }
-    }
-
-    /**
-     * Validate that the argument condition is {@code true}; otherwise
-     * throwing an exception with the specified message. This method is useful when
-     * validating according to an arbitrary boolean expression, such as validating a
-     * primitive number or using your own custom validation expression.
-     *
-     * <pre>Validate.isTrue(d &gt; 0.0, "The value must be greater than zero: &#37;s", d);</pre>
-     *
-     * <p>For performance reasons, the double value is passed as a separate parameter and
-     * appended to the exception message only in the case of an error.</p>
-     *
-     * @param expression the boolean expression to check
-     * @param message    the {@link String#format(String, Object...)} exception message if invalid, not null
-     * @param value      the value to append to the message when invalid
-     * @throws IllegalArgumentException if expression is {@code false}
-     * @see #isTrue(boolean)
-     * @see #isTrue(boolean, String, long)
-     * @see #isTrue(boolean, String, Object...)
-     */
-    public static void isTrue(final boolean expression, final String message, final double value) {
-        if (!expression) {
-            throw new IllegalArgumentException(String.format(message, Double.valueOf(value)));
-        }
-    }
-
-    /**
-     * Validate that the argument condition is {@code true}; otherwise
-     * throwing an exception with the specified message. This method is useful when
-     * validating according to an arbitrary boolean expression, such as validating a
-     * primitive number or using your own custom validation expression.
-     *
-     * <pre>
-     * Validate.isTrue(i &gt;= min &amp;&amp; i &lt;= max, "The value must be between &#37;d and &#37;d", min, max);</pre>
-     *
-     * @param expression the boolean expression to check
-     * @param message    the {@link String#format(String, Object...)} exception message if invalid, not null
-     * @param values     the optional values for the formatted exception message, null array not recommended
-     * @throws IllegalArgumentException if expression is {@code false}
-     * @see #isTrue(boolean)
-     * @see #isTrue(boolean, String, long)
-     * @see #isTrue(boolean, String, double)
-     */
     public static void isTrue(final boolean expression, final String message, final Object... values) {
         if (!expression) {
             throw new IllegalArgumentException(getMessage(message, values));
         }
-    }
-
-    /**
-     * Validate that the argument condition is {@code true}; otherwise
-     * throwing an exception. This method is useful when validating according
-     * to an arbitrary boolean expression, such as validating a
-     * primitive number or using your own custom validation expression.
-     *
-     * <pre>
-     * Validate.isTrue(i &gt; 0);
-     * Validate.isTrue(myObject.isOk());</pre>
-     *
-     * <p>The message of the exception is &quot;The validated expression is
-     * false&quot;.</p>
-     *
-     * @param expression the boolean expression to check
-     * @throws IllegalArgumentException if expression is {@code false}
-     * @see #isTrue(boolean, String, long)
-     * @see #isTrue(boolean, String, double)
-     * @see #isTrue(boolean, String, Object...)
-     */
-    public static void isTrue(final boolean expression) {
-        if (!expression) {
-            throw new IllegalArgumentException(DEFAULT_IS_TRUE_EX_MESSAGE);
-        }
-    }
-
-    /**
-     * Validate that the specified argument is not {@code null};
-     * otherwise throwing an exception with the specified message.
-     *
-     * <pre>Validate.notNull(myObject, "The object must not be null");</pre>
-     *
-     * @param <T>     the object type
-     * @param object  the object to check
-     * @param message the {@link String#format(String, Object...)} exception message if invalid, not null
-     * @param values  the optional values for the formatted exception message
-     * @return the validated object (never {@code null} for method chaining)
-     * @throws NullPointerException if the object is {@code null}
-     * @see Objects#requireNonNull(Object)
-     */
-    public static <T> T notNull(final T object, final String message, final Object... values) {
-        return Objects.requireNonNull(object, toSupplier(message, values));
     }
 
     private static Supplier<String> toSupplier(final String message, final Object... values) {
@@ -245,94 +131,6 @@ public class Validate {
      */
     public static <T> T[] notEmpty(final T[] array) {
         return notEmpty(array, DEFAULT_NOT_EMPTY_ARRAY_EX_MESSAGE);
-    }
-
-    /**
-     * <p>Validate that the specified argument collection is neither {@code null}
-     * nor a size of zero (no elements); otherwise throwing an exception
-     * with the specified message.
-     *
-     * <pre>Validate.notEmpty(myCollection, "The collection must not be empty");</pre>
-     *
-     * @param <T>        the collection type
-     * @param collection the collection to check, validated not null by this method
-     * @param message    the {@link String#format(String, Object...)} exception message if invalid, not null
-     * @param values     the optional values for the formatted exception message, null array not recommended
-     * @return the validated collection (never {@code null} method for chaining)
-     * @throws NullPointerException     if the collection is {@code null}
-     * @throws IllegalArgumentException if the collection is empty
-     * @see #notEmpty(Object[])
-     */
-    public static <T extends Collection<?>> T notEmpty(final T collection, final String message, final Object... values) {
-        Objects.requireNonNull(collection, toSupplier(message, values));
-        if (collection.isEmpty()) {
-            throw new IllegalArgumentException(getMessage(message, values));
-        }
-        return collection;
-    }
-
-    /**
-     * <p>Validate that the specified argument collection is neither {@code null}
-     * nor a size of zero (no elements); otherwise throwing an exception.
-     *
-     * <pre>Validate.notEmpty(myCollection);</pre>
-     *
-     * <p>The message in the exception is &quot;The validated collection is
-     * empty&quot;.
-     *
-     * @param <T>        the collection type
-     * @param collection the collection to check, validated not null by this method
-     * @return the validated collection (never {@code null} method for chaining)
-     * @throws NullPointerException     if the collection is {@code null}
-     * @throws IllegalArgumentException if the collection is empty
-     * @see #notEmpty(Collection, String, Object...)
-     */
-    public static <T extends Collection<?>> T notEmpty(final T collection) {
-        return notEmpty(collection, DEFAULT_NOT_EMPTY_COLLECTION_EX_MESSAGE);
-    }
-
-    /**
-     * Validate that the specified argument map is neither {@code null}
-     * nor a size of zero (no elements); otherwise throwing an exception
-     * with the specified message.
-     *
-     * <pre>Validate.notEmpty(myMap, "The map must not be empty");</pre>
-     *
-     * @param <T>     the map type
-     * @param map     the map to check, validated not null by this method
-     * @param message the {@link String#format(String, Object...)} exception message if invalid, not null
-     * @param values  the optional values for the formatted exception message, null array not recommended
-     * @return the validated map (never {@code null} method for chaining)
-     * @throws NullPointerException     if the map is {@code null}
-     * @throws IllegalArgumentException if the map is empty
-     * @see #notEmpty(Object[])
-     */
-    public static <T extends Map<?, ?>> T notEmpty(final T map, final String message, final Object... values) {
-        Objects.requireNonNull(map, toSupplier(message, values));
-        if (map.isEmpty()) {
-            throw new IllegalArgumentException(getMessage(message, values));
-        }
-        return map;
-    }
-
-    /**
-     * <p>Validate that the specified argument map is neither {@code null}
-     * nor a size of zero (no elements); otherwise throwing an exception.
-     *
-     * <pre>Validate.notEmpty(myMap);</pre>
-     *
-     * <p>The message in the exception is &quot;The validated map is
-     * empty&quot;.
-     *
-     * @param <T> the map type
-     * @param map the map to check, validated not null by this method
-     * @return the validated map (never {@code null} method for chaining)
-     * @throws NullPointerException     if the map is {@code null}
-     * @throws IllegalArgumentException if the map is empty
-     * @see #notEmpty(Map, String, Object...)
-     */
-    public static <T extends Map<?, ?>> T notEmpty(final T map) {
-        return notEmpty(map, DEFAULT_NOT_EMPTY_MAP_EX_MESSAGE);
     }
 
     /**
